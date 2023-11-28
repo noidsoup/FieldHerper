0781ebcc2058fdadabb2b16e6f4d3d23028b5825
noidsoup <nicholas.eymann@gmail.com>
2023-11-28 11:42:18 -0700

    adding proj

---
+++ herplistapp/herplistapp/herplistappApp.swift
@@ -0,0 +1,17 @@
+//
+//  herplistappApp.swift
+//  herplistapp
+//
+//  Created by nicholas eymann on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct herplistappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
